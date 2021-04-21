59c7d39a7724025f99efa039aa6acddb862c4cfc
Kartikgambhirr <44085519+Kartikgambhirr@users.noreply.github.com>
2021-04-21 21:35:17 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,42 @@
+<?php include "navtry.php" ?>
+<!DOCTYPE html>
+<head>
+    <title>About</title>
+</head>
+<body>
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h3 class="intro-text text-center " align="center" style="color: white">
+                        <strong>ABOUT</strong>
+                    </h3>
+                    <h5 align="center" style="color: white">
+                        This Website is developed by Kartik Gambhir And Tejus Sahi.<br> Website is made using PHP and MySQL.
+                    </h5>
+                    <hr>
+                </div>
+                <div class="col-md-8" align="center">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d875.009757955646!2d77.134488!3d28.688479!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x8e5be1770384217a!2sManorama%20Book%20Store!5e0!3m2!1sen!2sin!4v1571042880220!5m2!1sen!2sin" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+                </div>
+                <div class="col-md-4">
+                    <p style="color: white"><b>Phone:
+                        <strong>9876543210</strong>
+                    </p>
+                    <p style="color: white">Email:
+                        <strong>info@thebookstore.com</a></strong>
+                    </p>
+                    <p style="color: white">Address:
+                        <strong>LG-7 ,Vardhaman plaza, Rani Bagh Road, Pitampura ,
+                            <br> Delhi-110034</strong></b>
+                    </p>
+                </div>
+                <div class="clearfix" style="color: white"></div>
+            </div>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
